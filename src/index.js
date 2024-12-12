aa85327668bef4c16525480ae2018641860f5b33
Oliver <oliver.turner1@students.mq.edu.au>
2024-12-12 21:24:49 +1100

    first attempt at reading file functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@
 
 const inputFile = document.getElementByID("file");
 const preview = document.getElementByID("canvas");
-const contextPreview = preview.getContext("2d");
+const contextPreview = preview.getContext('2d');
 const reader = new FileReader();
 
+inputFile.onchange = function(e) {
+    const file = e.target.files[0];
+    
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+        const image = new Image();
+        image.onload = () => {
+            preview.width = image.width;
+            preview.height = image.height;
+
+            contextPreview.drawImage(image, 0, 0);
+        }
+
+        image.src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+};
