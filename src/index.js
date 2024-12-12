1ee4746373142b03a930440cf62e6d36a565b3d2
Oliver <oliver.turner1@students.mq.edu.au>
2024-12-12 16:40:47 +1100

    added DOM methods to access file input button and the canvas and pre tags

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // Read input file and display on canvas
+
+const inputFile = document.getElementByID("file");
+const preview = document.getElementByID("canvas");
+const contextPreview = preview.getContext("2d");
+const reader = new FileReader();
+
