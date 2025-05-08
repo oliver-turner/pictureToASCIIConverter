4524016fe88a0db8ae3adab66c7a9158e010e572
oturner01 <oliver.turner1@students.mq.edu.au>
2025-05-08 16:55:57 +1000

    read input file and display on canvas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // Read input file and display on canvas
 
-const inputFile = document.getElementByID("file");
-const preview = document.getElementByID("canvas");
+const inputFile = document.getElementById("file");
+const preview = document.getElementById("canvas");
 const contextPreview = preview.getContext('2d');
 const reader = new FileReader();
 
 inputFile.onchange = function(e) {
     const file = e.target.files[0];
     
-    reader.readAsDataURL(file);
     reader.onload = (event) => {
         const image = new Image();
         image.onload = () => {
