882f564df81b861dde070994a782a913e2e96962
oliver-turner <oliver01turner@gmail.com>
2025-05-14 17:49:25 +1000

    first attempt convert canvas to ascii

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const preview = document.getElementById("canvas");
 const contextPreview = preview.getContext('2d');
 const reader = new FileReader();
 
-inputFile.onchange = function(e) {
+const context =  canvas.getContext('2d');
+
+inputFile.onchange = (e) => {
     const file = e.target.files[0];
     
     reader.onload = (event) => {
@@ -13,6 +15,7 @@ inputFile.onchange = function(e) {
             preview.width = image.width;
             preview.height = image.height;
 
+            // Draw image onto canvas
             contextPreview.drawImage(image, 0, 0);
         }
 
@@ -22,7 +25,27 @@ inputFile.onchange = function(e) {
     reader.readAsDataURL(file);
 };
 
+const toAscii = (canvas) => {
+    const context = canvas.getContext('2d');
+    const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const pixelData = imgData.data;
+
+    for (let i = 0; i < pixelData.length; i += 4) {
+        const red = pixelData[i];
+        const green = pixelData[i + 1];
+        const blue = pixelData[i + 2];
+
+        const grayscaleBrightness = 0.299 * red + 0.587 * green + 0.114 * blue;
+
+        
+    }
+
+} 
+
 
 // Read pixel data using canvas.getContext("2d).getImageData()
+
+
+
 // Convert brightness levels to characters using a predefined ASCII ramp
 // Render output as <pre>
